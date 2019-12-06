c16bd4dcd1adf28893ad62b3073b433c601d419d
Arlind Hajdari <arlind.hajdari@hotmail.com>
2019-12-06 22:19:07 +0100

    HybridRecommenderSystem untested

--- HRS.py
+++ HRS.py
@@ -1,64 +1,101 @@
-import numpy as np
 import pandas as pd
 from surprise import SVD, Reader, Dataset
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-#Import or compute the cosine_sim matrix
-cosine_sim = pd.read_csv('../data/cosine_sim.csv')
+RATINGS_SMALL = "../data/ratings_small.csv"
+MOVIES = "../data/movie_ids.csv"
+METADATA_CLEAN = "../data/metadata_clean.csv"
+ORIGINAL_DATASET = "../data/movies_metadata.csv"
 
-#Import or compute the cosine sim mapping matrix
-cosine_sim_map = pd.read_csv('../data/cosine_sim_map.csv', header=None)
 
-#Convert cosine_sim_map into a Pandas Series
-cosine_sim_map = cosine_sim_map.set_index(0)
-cosine_sim_map = cosine_sim_map[1]
+class HybridRecommenderSystem:
+    def __init__(self, movies_path=MOVIES,
+                 clean_dataset_path=METADATA_CLEAN, original_dataset_path=ORIGINAL_DATASET):
+        self.svd = SVD()
+        self.reader = Reader()
+        id_map = pd.read_csv(movies_path)
+        self.id_to_title = id_map.set_index('id')
+        self.clean_dataset_path = clean_dataset_path
+        self.original_dataset_path = original_dataset_path
 
-reader = Reader()
-ratings = pd.read_csv('../data/ratings_small.csv')
-data = Dataset.load_from_df(ratings[['userId', 'movieId', 'rating']], reader)
-data.split(n_folds=5)
-svd = SVD()
-trainset = data.build_full_trainset()
-svd.train(trainset)
+    def initialize(self):
+        data = self.load_ratings(RATINGS_SMALL)
+        self.train_svd(data)
+        self.clean_dataframe = pd.read_csv(self.clean_dataset_path)  # Clean dataset
+        self.original_dataframe = pd.read_csv(self.original_dataset_path, low_memory=False)  # Original dataset
 
-#Build title to ID and ID to title mappings
-id_map = pd.read_csv('../data/movie_ids.csv')
-id_to_title = id_map.set_index('id')
-title_to_id = id_map.set_index('title')
+    def load_ratings(self, csv_path):
+        ratings = pd.read_csv(csv_path)
+        data = Dataset.load_from_df(ratings[['userId', 'movieId', 'rating']], self.reader)
+        return data
 
-#Import or compute relevant metadata of the movies
-smd = pd.read_csv('../data/metadata_small.csv')
+    def train_svd(self, dataset: Dataset):
+        trainset = dataset.build_full_trainset()
+        self.svd.fit(trainset)
 
-def hybrid(userId, title):
-    #Extract the cosine_sim index of the movie
-    idx = cosine_sim_map[title]
-    
-    #Extract the TMDB ID of the movie
-    tmdbId = title_to_id.loc[title]['id']
-    
-    #Extract the movie ID internally assigned by the dataset
-    movie_id = title_to_id.loc[title]['movieId']
-    
-    #Extract the similarity scores and their corresponding index for every movie from the cosine_sim matrix
-    sim_scores = list(enumerate(cosine_sim[str(int(idx))]))
-    
-    #Sort the (index, score) tuples in decreasing order of similarity scores
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    
-    #Select the top 25 tuples, excluding the first 
-    #(as it is the similarity score of the movie with itself)
-    sim_scores = sim_scores[1:26]
-    
-    #Store the cosine_sim indices of the top 25 movies in a list
-    movie_indices = [i[0] for i in sim_scores]
+    def calculate_tfidf(self):
+        # Add extra columns to clean_dataset from orignial's one
+        self.clean_dataframe['overview'], self.clean_dataframe['id'] = self.original_dataframe['overview'], self.original_dataframe['id']
 
-    #Extract the metadata of the aforementioned movies
-    movies = smd.iloc[movie_indices][['title', 'vote_count', 'vote_average', 'year', 'id']]
-    
-    #Compute the predicted ratings using the SVD filter
-    movies['est'] = movies['id'].apply(lambda x: svd.predict(userId, id_to_title.loc[x]['movieId']).est)
-    
-    #Sort the movies in decreasing order of predicted rating
-    movies = movies.sort_values('est', ascending=False)
-    
-    #Return the top 10 movies as recommendations
-    return movies.head(10)
+        # Define a TF-IDF Vectorizer Object
+        tfidf = TfidfVectorizer(stop_words='english')  # Remove all english stopwords
+
+        # Replace NaN with an empty string
+        self.clean_dataframe['overview'] = self.clean_dataframe['overview'].fillna('')
+
+        # Construct the required TF-IDF matrix by applying the fit_transform method on the overview feature
+        tfidf_matrix = tfidf.fit_transform(self.clean_dataframe['overview'])
+
+        return tfidf_matrix
+
+    def calculate_cosine_similarity(self):
+        tfidf_matrix = self.calculate_tfidf()
+
+        cosine_similarity = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+        return cosine_similarity
+
+    def generate_indicies(self, movies_dataset):
+        indices = pd.Series(movies_dataset.index, index=movies_dataset['title']).drop_duplicates()
+        return indices
+
+    def content_based_recommender(self, title, movies_dataset):
+        # Obtain the index of the movie that matches the title
+        indices = self.generate_indicies(movies_dataset)
+        idx = indices[title]
+
+        # Calculate cosine similarity
+        cosine_similarity = self.calculate_cosine_similarity()
+
+        # Get the pairwsie similarity scores of all movies with that movie
+        # And convert it into a list of tuples as described above
+        sim_scores = list(enumerate(cosine_similarity[idx]))
+
+        # Sort the movies based on the cosine similarity scores
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+        # Get the scores of the 10 most similar movies. Ignore the first movie.
+        sim_scores = sim_scores[1:26]
+
+        # Get the movie indices
+        movie_indices = [i[0] for i in sim_scores]
+
+        # Return the top 10 most similar movies
+        return movies_dataset['title'].iloc[movie_indices]
+
+    def hybrid(self, userId, title):
+        movie_indices = self.content_based_recommender(title, self.original_dataframe)
+
+        #Extract the metadata of the aforementioned movies
+        movies = self.original_dataframe.iloc[movie_indices][['title', 'vote_count', 'vote_average', 'year', 'id']]
+
+        #Compute the predicted ratings using the SVD filter
+        movies['est'] = movies['id'].apply(lambda x: self.svd.predict(userId, self.id_to_title.loc[x]['title']).est)
+
+        #Sort the movies in decreasing order of predicted rating
+        movies = movies.sort_values('est', ascending=False)
+
+        #Return the top 10 movies as recommendations
+        return movies.head(10)
+
---
+++ helper.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+from ast import literal_eval
+
+
+# Helper function to convert NaT to 0 and all other years to integers.
+def convert_int(x):
+    try:
+        return int(x)
+    except:
+        return 0
+
+
+def clean_dataset(self, full_dataset_path="../data/movies_metadata.csv"):
+    full_dataset = pd.read_csv(full_dataset_path)
+
+    # Only keep those features that we require
+    full_dataset = full_dataset[['title', 'genres', 'release_date', 'runtime', 'vote_average', 'vote_count']]
+
+    # Convert release_date into pandas datetime format
+    full_dataset['release_date'] = pd.to_datetime(full_dataset['release_date'], errors='coerce')
+
+    # Extract year from the datetime
+    full_dataset['year'] = full_dataset['release_date'].apply(lambda x: str(x).split('-')[0] if x != np.nan else np.nan)
+
+    # Apply convert_int to the year feature
+    full_dataset['year'] = full_dataset['year'].apply(self.convert_int)
+
+    # Drop the release_date column
+    full_dataset = full_dataset.drop('release_date', axis=1)
+
+    # Convert all NaN into stringified empty lists
+    full_dataset['genres'] = full_dataset['genres'].fillna('[]')
+
+    # Apply literal_eval to convert stringified empty lists to the list object
+    full_dataset['genres'] = full_dataset['genres'].apply(literal_eval)
+
+    # Convert list of dictionaries to a list of strings
+    full_dataset['genres'] = full_dataset['genres'].apply(lambda x: [i['name'].lower() for i in x] if isinstance(x, list) else [])
+
+    return full_dataset
